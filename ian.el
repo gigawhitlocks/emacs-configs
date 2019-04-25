24854b58d18e42a668a0bad48a60c78799475a75
Ian Whitlock <iwhitlock@vmware.com>
2019-04-25 01:10:11 -0500

    Counsel, swiper, ivy, and projectile.

--- ian.el
+++ ian.el
@@ -29,6 +29,13 @@
 (defun global-packages ()
   "Install and configure packages used with many modes and standalone modes and applications."
 
+  (defun setup-projectile ()
+    (use-package projectile)
+    (projectile-mode +1)
+    (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+    )
+
   (defun setup-evil ()
     "Install and configure evil-mode and related bindings."
     (use-package evil
@@ -56,10 +63,36 @@
     )
 
   (defun setup-ivy ()
+    "Installs Ivy with a suggested config."
+
+    ;; I've lifted this sample config from the github readme for Ivy
     (use-package ivy)
-    (ivy-mode 1))
+    (use-package counsel)
+    (use-package swiper)
+    (ivy-mode 1)
+
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+
+    ;; Some global bindings:
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    ;; (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    ;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    ;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    ;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
+    ;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    ;; (global-set-key (kbd "C-x l") 'counsel-locate)
+    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
   (defun extra-packages ()
+    (use-package treemacs)
     (use-package restart-emacs)
     (use-package leuven-theme))
 
@@ -118,6 +151,8 @@
   (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 
+  ;; line numbers by default
+  (global-linum-mode 1)
   )
 
 (defun main()
