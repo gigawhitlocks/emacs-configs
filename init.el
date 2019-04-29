12648b8963613e814f1c691b54eaee3b8880305d
Ian Whitlock <iwhitlock@vmware.com>
2019-04-29 12:05:22 -0500

    Remove custom- from init.el

--- init.el
+++ init.el
@@ -11,17 +11,5 @@
 (require '~/.emacs.d/ian.el)
 (main)
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-   (lsp-scala sbt-mode scala-mode dockerfile-mode lsp-origami company-lsp lsp-ui lsp-mode treemacs origami leuven-theme color-theme-sanityinc-tomorrow restart-emacs helm evil-magit magit projectile evil-leader evil-escape evil-collection evil flycheck company which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;; init.el ends here
