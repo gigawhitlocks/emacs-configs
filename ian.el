25cf3a99bbec17a5b9fd8de48c239dbedfd61714
Ian Whitlock <ian@theknown.net>
2019-04-30 09:48:46 -0500

    Fix helm tab completion primarily

--- ian.el
+++ ian.el
@@ -140,8 +140,8 @@
       :delight
       :config
       (global-set-key (kbd "M-x") #'helm-M-x)
-      (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-      (global-set-key (kbd "C-x C-f") #'helm-find-files)
+      (add-to-list 'evil-collection-mode-list 'helm-mode)
+      (define-key helm-find-files-map "\t" 'helm-execute-persistent-action)
       (helm-mode 1)))
   
   (setup-evil)
@@ -228,7 +228,7 @@
     ;; buffer control
     "bb"	'switch-to-buffer
     "TAB"	'switch-to-prev-buffer
-    "bd"	'kill-buffer
+    "bd"	'kill-buffer-ask
 
     ;; errors
     "ec"        'flycheck-clear
