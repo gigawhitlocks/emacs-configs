8b194f8acb1319b373f40d130cf3041e2221be90
Ian Whitlock <ian@theknown.net>
2019-05-01 01:26:37 -0500

    Org working with tangle and linking

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; [[file:~/.emacs.d/ian.org::*Init%20file][Init file:1]]
 ;;; init --- the Emacs entrypoint
 ;;; Commentary:
 ;;;
@@ -5,20 +6,19 @@
 ;;; https://orgmode.org/worg/org-contrib/babel/intro.html#literate-emacs-init
 ;;;
 ;;; Code:
-
 ;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
 (let* ((org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" dotfiles-dir))))
+		 "lisp" (expand-file-name
+			 "org" (expand-file-name
+				"src" dotfiles-dir))))
        (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
+			 "lisp" (expand-file-name
+				 "contrib" (expand-file-name
+					    ".." org-dir))))
        (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
+			  (or load-path nil))))
   ;; load up Org-mode and Org-babel
   (require 'org-install)
   (require 'ob-tangle))
@@ -28,4 +28,9 @@
 
 (require '~/.emacs.d/ian.el)
 (main)
+
+;; Load automatic and interactive customizations from this computer
+(setq custom-file "~/.emacs-custom.el")
+(load custom-file)
 (provide 'init)
+;; Init file:1 ends here
