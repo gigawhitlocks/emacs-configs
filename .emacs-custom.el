5614b1d4b74bb047491db0735173804ace62e61c
Ian Whitlock <ian@theknown.net>
2022-01-18 17:49:59 -0600

    ignore session files; check in custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,11 +3,49 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(custom-safe-themes
-   '("c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
+   '("246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
+ '(exwm-floating-border-color "#1c1f24")
+ '(fci-rule-color "#62686E")
  '(helm-minibuffer-history-key "M-p")
+ '(highlight-tail-colors
+   ((("#2c3636" "#99bb66" "green")
+     . 0)
+    (("#293b44" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
+ '(objed-cursor-color "#ff665c")
  '(package-selected-packages
-   '(helm-ag salt-mode tree-sitter-langs tree-sitter sunshine elpher emacs-everywhere orderless unordered embark marginalia marginalize vertico consult htmlize ox-gfm org-download evil-org adaptive-wrap web-mode rust-mode gorepl-mode gotest flycheck-golangci-lint yasnippet-snippets yaml-mode which-key use-package-ensure-system-package undo-tree treemacs-projectile systemd restart-emacs ranger rainbow-delimiters moe-theme lsp-ui lsp-treemacs lsp-origami hyperbole helm-projectile helm-lsp helm-descbinds go-mode git-timemachine git-gutter general forge flycheck figlet exec-path-from-shell evil-surround evil-escape evil-collection emojify dockerfile-mode diminish delight dashboard company auto-virtualenv all-the-icons)))
+   '(helm-ag salt-mode tree-sitter-langs tree-sitter sunshine elpher emacs-everywhere orderless unordered embark marginalia marginalize vertico consult htmlize ox-gfm org-download evil-org adaptive-wrap web-mode rust-mode gorepl-mode gotest flycheck-golangci-lint yasnippet-snippets yaml-mode which-key use-package-ensure-system-package undo-tree treemacs-projectile systemd restart-emacs ranger rainbow-delimiters moe-theme lsp-ui lsp-treemacs lsp-origami hyperbole helm-projectile helm-lsp helm-descbinds go-mode git-timemachine git-gutter general forge flycheck figlet exec-path-from-shell evil-surround evil-escape evil-collection emojify dockerfile-mode diminish delight dashboard company auto-virtualenv all-the-icons))
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
+ '(rustic-ansi-faces
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
+ '(vc-annotate-background "#242730")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7bc275")
+    (cons 40 "#a6c677")
+    (cons 60 "#d1ca79")
+    (cons 80 "#FCCE7B")
+    (cons 100 "#f4b96e")
+    (cons 120 "#eda461")
+    (cons 140 "#e69055")
+    (cons 160 "#db8981")
+    (cons 180 "#d082ae")
+    (cons 200 "#C57BDB")
+    (cons 220 "#d874b0")
+    (cons 240 "#eb6d86")
+    (cons 260 "#ff665c")
+    (cons 280 "#d15e59")
+    (cons 300 "#a35758")
+    (cons 320 "#754f56")
+    (cons 340 "#62686E")
+    (cons 360 "#62686E")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
