0a7deee5e236bb1257a040a9f50c89faac1b17df
Ian Whitlock <ian@theknown.net>
2022-11-10 10:12:23 -0600

    Remove sublimity-mode
    feh

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -31,7 +31,8 @@
  '(rustic-ansi-faces
    ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(safe-local-variable-values
-   '((go-test-args . "-test.db.migrations=/home/ian/addr-api/deployments/migrations")))
+   '((go-test-args . "-failfast -test.db.migrations=/home/ian/addr-api/deployments/migrations")
+     (go-test-args . "-test.db.migrations=/home/ian/addr-api/deployments/migrations")))
  '(vc-annotate-background "#242730")
  '(vc-annotate-color-map
    (list
