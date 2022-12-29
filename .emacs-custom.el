d70ffb3b478084b69b12536fab859659ca6163b8
Ian Whitlock <ian@theknown.net>
2022-12-29 14:16:54 -0600

    new username

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -26,12 +26,12 @@
  '(objed-cursor-color "#ff665c")
  '(org-agenda-files '("~/notes/notes.org" "/home/ian/.emacs.d/ian.org"))
  '(package-selected-packages
-   '(sql-indent ox-jira ace-jump-mode ace-jump ack pc-bufsw doom-modeline treemacs-all-the-icons all-the-icons yasnippet-snippets which-key wgrep-ack web-mode use-package-ensure-system-package undo-tree tree-sitter-langs systemd solaire-mode salt-mode rust-mode restart-emacs rainbow-delimiters ox-gfm org-web-tools org-download lsp-ui lsp-origami json-mode hyperbole htmlize highlight-indent-guides helm-projectile helm-lsp helm-descbinds helm-ag gotest gorepl-mode git-timemachine git-gutter general forge flycheck-golangci-lint fira-code-mode figlet exec-path-from-shell evil-surround evil-org evil-escape evil-collection emojify doom-themes dockerfile-mode diminish delight dashboard company-org-block auto-virtualenv adaptive-wrap))
+   '(polymode sql-indent ox-jira ace-jump-mode ace-jump ack pc-bufsw doom-modeline treemacs-all-the-icons all-the-icons yasnippet-snippets which-key wgrep-ack web-mode use-package-ensure-system-package undo-tree tree-sitter-langs systemd solaire-mode salt-mode rust-mode restart-emacs rainbow-delimiters ox-gfm org-web-tools org-download lsp-ui lsp-origami json-mode hyperbole htmlize highlight-indent-guides helm-projectile helm-lsp helm-descbinds helm-ag gotest gorepl-mode git-timemachine git-gutter general forge flycheck-golangci-lint fira-code-mode figlet exec-path-from-shell evil-surround evil-org evil-escape evil-collection emojify doom-themes dockerfile-mode diminish delight dashboard company-org-block auto-virtualenv adaptive-wrap))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
  '(rustic-ansi-faces
    ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(safe-local-variable-values
-   '((go-test-args . "-test.db.migrations=/home/ian/addr-api/deployments/migrations")))
+   '((go-test-args . "-failfast -test.db.migrations=/home/iwhitlock/addr-api/deployments/migrations")))
  '(vc-annotate-background "#242730")
  '(vc-annotate-color-map
    (list
