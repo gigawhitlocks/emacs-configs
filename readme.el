f0af8daad80c4bc87dd98a69a19bb787547ed2b1
Ian Whitlock <ian@theknown.net>
2024-11-09 19:54:15 -0600

    remove HTML and Markdown generation and representations

--- readme.el
+++ readme.el
@@ -1,3 +1,3 @@
-;; exists to allow a simple loading of all modules with .org files, however readme.org is tangled to ian.el for historical and stubborn narcissist author reasons, so this file has to exist and do nothing to let Emacs start.
+;; exists to allow a simple loading of all modules with .org files with the routine I've had in my config forever, without modification, because it's a simpel workaround and I don't want to spend more time thinking about the code that is there.
 
-;; Is this stupid? Yes. Am I going to rewrite init.el or ian.el? No.
+;; Is this stupid? Yes. Am I going to rewrite my config? No.
