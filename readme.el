24935b77af4bbe913dde5764cf1656d39a06e817
Ian Whitlock <ian@theknown.net>
2024-11-09 19:45:05 -0600

    move ian.org to readme.org

---
+++ readme.el
@@ -0,0 +1,3 @@
+;; exists to allow a simple loading of all modules with .org files, however readme.org is tangled to ian.el for historical and stubborn narcissist author reasons, so this file has to exist and do nothing to let Emacs start.
+
+;; Is this stupid? Yes. Am I going to rewrite init.el or ian.el? No.
