02b769099e167c98725c99829099d90d11341816
Ian Whitlock <ian@theknown.net>
2025-10-07 21:23:44 -0700

    add a comment about why package.el is disabled

--- early-init.el
+++ early-init.el
@@ -1,3 +1,4 @@
 ;; a bugfix for a warning that appears at startup saying 'package cl is deprecated'
 (setq byte-compile-warnings '(not obsolete))
+;; for elpaca
 (setq package-enable-at-startup nil)
