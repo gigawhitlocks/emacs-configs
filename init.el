e15ec939df4db9cb8aa71c6d1e168064fed716b7
Ian Whitlock <github@theknown.net>
2026-04-07 10:51:09 -0700

    fixed org-mode version mismatch elpa/builtin

--- init.el
+++ init.el
@@ -2,17 +2,10 @@
       (file-name-directory
        (or (buffer-file-name) load-file-name)))
 
-(let* ((org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
-  ;; load up Org-mode and Org-babel
+(let* ((elpaca-org-dir (expand-file-name "elpaca/builds/org" dotfiles-dir))
+       (load-path (if (file-directory-p elpaca-org-dir)
+                      (cons elpaca-org-dir (or load-path nil))
+                    (or load-path nil))))
   (require 'ob-tangle))
 
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
